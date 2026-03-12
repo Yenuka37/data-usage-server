1ffabfcaf0a9d27918f80d2a2773403842965f3e
Yenuka37 <yenukaharshan37@gmail.com>
2026-03-12 09:54:00 +0530

    fix some errors

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path    = require("path");
 const usageRoutes = require("./routes/usage");
 
 const app  = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8080;
 
 // ── Middleware ──────────────────────────────────────────────────────────────
 app.use(cors({
